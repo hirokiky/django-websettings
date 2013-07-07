60855ae39c148ce7201d2765094df1a9ec845d8f
hirokiky <hirokiky@gmail.com>
2013-07-07 16:22:37 +0900

    Fixed package name.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 
-setup(name='websettings',
+setup(name='django-websettings',
       version='0.0.1',
       description="Django application to provide a web interface "
                   "to set a yet another django's settings",
