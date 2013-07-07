404e2f8aa1d583dec97956679d8fa30e7fd633a2
hirokiky <hirokiky@gmail.com>
2013-07-07 23:12:37 +0900

    Added tests for db backend.

--- websettings/tests/tests.py
+++ websettings/tests/tests.py
@@ -2,8 +2,48 @@ from django.test import TestCase
 
 
 __all__ = (
+    'DBBackendTest',
     'DBSettingStoreTest',
 )
+
+
+class DBBackendTest(TestCase):
+    def _getTarget(self):
+        from websettings.backends import db
+        return db
+
+    def _set_setting(self, **kwargs):
+        from websettings.models import Setting
+        return Setting.objects.create(**kwargs)
+
+    def _get_setting(self, **kwargs):
+        from websettings.models import Setting
+        return Setting.objects.get(**kwargs)
+
+    def test_getsetting_with_setting(self):
+        self._set_setting(key='BOSS', value='ritsu')
+        target = self._getTarget()
+        result = target.getsetting('BOSS')
+        self.assertEqual('ritsu', result)
+
+    def test_getsetting_without_setting(self):
+        target = self._getTarget()
+        self.assertRaises(AttributeError, target.getsetting, 'BOSS')
+
+    def test_setsetting_with_setting(self):
+        self._set_setting(key='BOSS', value='ritsu')
+        target = self._getTarget()
+        target.setsetting('BOSS', 'mio')
+
+        result = self._get_setting(key='BOSS')
+        self.assertEqual(result.value, 'mio')
+
+    def test_setting_without_setting(self):
+        target = self._getTarget()
+        target.setsetting('BOSS', 'ritsu')
+
+        result = self._get_setting(key='BOSS')
+        self.assertEqual(result.value, 'ritsu')
 
 
 class DBSettingStoreTest(TestCase):
