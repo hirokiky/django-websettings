63234672c1a1473f998a3215218a42684034f3de
hirokiky <hirokiky@gmail.com>
2013-07-08 01:14:52 +0900

    Fixed SettingStoreForm for more testable.

--- websettings/forms.py
+++ websettings/forms.py
@@ -6,20 +6,21 @@ from django.utils.datastructures import SortedDict
 from websettings import websettings
 
 
-def get_settingstore_fields():
+def get_settingstore_fields(setting_store):
     fields = [(k, forms.CharField(max_length=255,
                                   label=k,
-                                  initial=partial(getattr, websettings, k)))
-              for k in websettings.settings.keys()]
+                                  initial=partial(getattr, setting_store, k)))
+              for k in setting_store.settings.keys()]
     return SortedDict(fields)
 
 
 class SettingStoreFieldsMetaclass(type):
-    def __new__(cls, *args, **kwargs):
+    def __new__(cls, name, bases, attrs):
         super_new = super(SettingStoreFieldsMetaclass, cls).__new__
-        new_class = super_new(cls, *args, **kwargs)
+        new_class = super_new(cls, name, bases, attrs)
 
-        new_class.base_fields = get_settingstore_fields()
+        setting_store = attrs.get('setting_store', websettings)
+        new_class.base_fields = get_settingstore_fields(setting_store)
         return new_class
 
 
@@ -27,7 +28,7 @@ class BaseSettingStoreForm(forms.BaseForm):
     def save(self):
         if hasattr(self, 'cleaned_data'):
             for k, v in self.cleaned_data.items():
-                setattr(websettings, k, v)
+                setattr(self.setting_store, k, v)
         else:
             # TODO: need more humanization.
             raise ValueError('Form needs cleaned_data')
@@ -35,3 +36,6 @@ class BaseSettingStoreForm(forms.BaseForm):
 
 class SettingStoreForm(BaseSettingStoreForm):
     __metaclass__ = SettingStoreFieldsMetaclass
+
+    # For more testable
+    setting_store = websettings
